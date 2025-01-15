6bf558531dfadbf19d28b7ea9f462d5b9880b71e
Brian Shewan <brian.shewan@nscc.ca>
2025-01-15 10:28:39 -0400

    added command line example

---
+++ command_line/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main(int argc, char** argv, char** envp) {
+    // list all arguments
+    const std::vector<std::string_view> args(argv + 1, argv + argc);
+    for (const auto& arg : args) {
+        std::cout << arg << std::endl;
+    }
+
+    // output environment variables
+    std::stringstream ss;
+    std::string name, value;
+    for (auto e = envp; *e != nullptr; e++) {
+        ss.clear();
+        ss << *e;
+        std::getline(ss, name, '=');
+        std::getline(ss, value);
+        if (name == "THEME") {
+            std::cout << *e << std::endl;
+            std::cout << "Name: " << name << std::endl;
+            std::cout << "Value: " << value << std::endl;
+        }
+    }
+
+    return 0;
+}
