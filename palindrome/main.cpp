6da0023e60e83294df0fcbeaf57723ee1ba497ac
Brian Shewan <brian.shewan@nscc.ca>
2025-03-20 09:39:18 -0300

    added palindrome examples

--- palindrome/main.cpp
+++ palindrome/main.cpp
@@ -10,12 +10,40 @@ bool is_palindrome(const char* input, const size_t length) {
     return true;
 }
 
+bool is_palindrome_recursive(const char* input, const size_t length) {
+    if (length <= 1) return false;
+    if (input[0] == input[length - 1]) {
+        return is_palindrome(&input[1], length - 2);
+    }
+    return false;
+}
+
 bool is_palindrome2(std::string_view input) {
     while (input.size() > 1) {
         if (input[0] != input[input.length() - 1]) { return false; }
         input = std::string_view(input.data() + 1, input.length() - 2);
     }
     return true;
+}
+
+// tail recursion (recursion done last)
+bool is_palindrome_recursive2(std::string_view input) {
+    if (input.length() <= 1) return true;
+
+    if (*input.begin() == *input.rbegin()) {
+        return is_palindrome_recursive2({input.data() + 1, input.length() - 2});
+    }
+    return false;
+}
+
+// head recursion (recursion done first)
+bool is_palindrome_recursive3(std::string_view input) {
+    if (input.length() <= 1) return true;
+
+    if (is_palindrome_recursive3({input.data() + 1, input.length() - 2})) {
+        return *input.begin() == *input.rbegin();
+    }
+    return false;
 }
 
 bool is_palindrome3(std::string_view input) {
@@ -36,8 +64,29 @@ int main() {
         std::cout << buffer << " is NOT a palindrome" << std::endl;
     }
 
+    std::cout << "c string version (recursive)" << std::endl;
+    if (is_palindrome_recursive(buffer, strnlen(buffer, BUFFER_SIZE))) {
+        std::cout << buffer << " is a palindrome" << std::endl;
+    } else {
+        std::cout << buffer << " is NOT a palindrome" << std::endl;
+    }
+
     std::cout << "std string version" << std::endl;
     if (is_palindrome2(buffer)) {
+        std::cout << buffer << " is a palindrome" << std::endl;
+    } else {
+        std::cout << buffer << " is NOT a palindrome" << std::endl;
+    }
+
+    std::cout << "std string version (recursive) - tail version" << std::endl;
+    if (is_palindrome_recursive2(buffer)) {
+        std::cout << buffer << " is a palindrome" << std::endl;
+    } else {
+        std::cout << buffer << " is NOT a palindrome" << std::endl;
+    }
+
+    std::cout << "std string version (recursive) - head version" << std::endl;
+    if (is_palindrome_recursive3(buffer)) {
         std::cout << buffer << " is a palindrome" << std::endl;
     } else {
         std::cout << buffer << " is NOT a palindrome" << std::endl;
